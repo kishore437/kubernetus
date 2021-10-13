1fddd2663af7e895c83ed5155cd240dbd3e447a3
kishore437 <32472221+kishore437@users.noreply.github.com>
2021-10-12 20:00:27 -0500

    Update python.py

--- python.py
+++ python.py
@@ -9,3 +9,17 @@ a = open('hi.txt', 'r')
 b = a.read()
 print(b)
 a.close()
+
+class kishore:
+    def __init__(self,brand,battery,ram,camera):
+        self.Brand=brand
+        self.battery=battery
+        self.ram=ram
+        self.camera=camera
+    def display(self):
+        print("Brand:",self.Brand)
+        print("Battery:",self.battery)
+        print("ram:",self.ram)
+        print("camera:",self.camera)
+obj=kishore("apple",'4000mah','8gb','48mp',)
+obj.display()
