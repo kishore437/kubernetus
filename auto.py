4d4c2d8cd507b238a8d5d7045bde3ead66d28bc7
Dantuluri <kdantuluri@dxc.com>
2021-10-13 11:18:16 -0500

    test files

---
+++ auto.py
@@ -0,0 +1,30 @@
+
+# file handling
+
+a = open('hi.txt', 'w')
+b = a.write('this is day28')
+print(b)
+a.close()
+
+a = open('hi.txt', 'r')
+b = a.read()
+print(b)
+a.close()
+
+class kishore:
+    def __init__(self,brand,battery,ram,camera):
+        self.Brand=brand
+        self.battery=battery
+        self.ram=ram
+        self.camera=camera
+    def display(self):
+        print("Brand:",self.Brand)
+        print("Battery:",self.battery)
+        print("ram:",self.ram)
+        print("camera:",self.camera)
+obj=kishore("apple",'4000mah','8gb','48mp',)
+obj.display()
+
+
+
+
